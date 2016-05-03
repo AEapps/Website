d2d4cebefe1654388b01780515eb02ff71712360
Amitai5 <amitaierfanian25@gmail.com>
2016-05-02 21:58:24 -0700

    Split files further
    Split CSS files down into more specific files. Also split JS into JS
    files per page.

---
+++ AEapps/Javascript/Notepad.js
@@ -0,0 +1,22 @@
+﻿function DownloadText(TextBoxID) {
+    var textToWrite = document.getElementById(TextBoxID).value;
+    var TextFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
+    var FileName = "Note.txt";
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = FileName;
+    downloadLink.innerHTML = "Hidden Link";
+
+    window.URL = window.URL || window.webkitURL;
+
+    downloadLink.href = window.URL.createObjectURL(TextFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+
+    downloadLink.click();
+}
+
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
+}
