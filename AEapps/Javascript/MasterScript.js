4103c8d79a48bac27a47c201e853173b19f10899
Amitai5 <amitaierfanian25@gmail.com>
2016-06-12 14:07:59 -0700

    Made The Page Link Color Function More Efficent And Faster.

--- AEapps/Javascript/MasterScript.js
+++ AEapps/Javascript/MasterScript.js
@@ -3,24 +3,24 @@
 }
 
 function PageLinkColors() {
-    //Page Link Stuff
-    var sPath = window.location.pathname;
-    var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
-    if (sPage == "Home.aspx" || sPage == "Home.phtml" || sPage == "Home.html") {
-        document.getElementById("HomeLink").classList.remove("h2NavLink");
-        document.getElementById("HomeLink").classList.add("NavLinkWhite");
-    }
-    else if (sPage == "Our%20Apps.aspx" || sPage == "Our%20Apps.phtml" || sPage == "Our%20Apps.html") {
-        document.getElementById("AppsLink").classList.remove("h2NavLink");
-        document.getElementById("AppsLink").classList.add("NavLinkWhite");
-    }
-    else if (sPage == "About%20Us.aspx" || sPage == "About%20Us.phtml" || sPage == "About%20Us.html") {
-        document.getElementById("AboutLink").classList.remove("h2NavLink");
-        document.getElementById("AboutLink").classList.add("NavLinkWhite");
-    }
-    else if (sPage == "Contact%20Us.aspx" || sPage == "Contact%20Us.phtml" || sPage == "Contact%20Us.html") {
-        document.getElementById("ContactLink").classList.remove("h2NavLink");
-        document.getElementById("ContactLink").classList.add("NavLinkWhite");
+    switch(document.getElementsByTagName("title")[0].innerText)
+    {
+        case "AEapps | Home":
+            document.getElementById("HomeLink").classList.remove("h2NavLink");
+            document.getElementById("HomeLink").classList.add("NavLinkWhite");
+            break;
+        case "AEapps | Our Apps":
+            document.getElementById("AppsLink").classList.remove("h2NavLink");
+            document.getElementById("AppsLink").classList.add("NavLinkWhite");
+            break;
+        case "AEapps | About Us":
+            document.getElementById("AboutLink").classList.remove("h2NavLink");
+            document.getElementById("AboutLink").classList.add("NavLinkWhite");
+            break;
+        case "AEapps | Contact Us":
+            document.getElementById("ContactLink").classList.remove("h2NavLink");
+            document.getElementById("ContactLink").classList.add("NavLinkWhite");
+            break;
     }
 }
 
@@ -31,5 +31,4 @@ function CopyText(ElementID) {
     aux.select();
     document.execCommand("copy");
     document.body.removeChild(aux);
-
 }
